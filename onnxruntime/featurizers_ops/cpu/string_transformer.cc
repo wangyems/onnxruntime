713324a191c53aeb6c17fc9825bbc109e357368b
wangyems <wangye@microsoft.com>
2020-03-01 23:25:21 -0800

    update

--- onnxruntime/featurizers_ops/cpu/string_transformer.cc
+++ onnxruntime/featurizers_ops/cpu/string_transformer.cc
@@ -12,22 +12,50 @@
 namespace onnxruntime {
 namespace featurizers {
 
-template <typename InputT>
-struct StringTransformerImpl {
-  void operator()(OpKernelContext* ctx) const {
+// template <typename InputT>
+// struct StringTransformerImpl {
+//   void operator()(OpKernelContext* ctx) const {
+//     // Create the transformer
+//     Microsoft::Featurizer::Featurizers::StringTransformer<InputT> transformer(
+//         [ctx](void) {
+//           const auto* state_tensor(ctx->Input<Tensor>(0));
+//           const uint8_t* const state_data(state_tensor->Data<uint8_t>());
+
+//           Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().GetDims()[0]);
+//           return Microsoft::Featurizer::Featurizers::StringTransformer<InputT>(archive);
+//         }());
+
+//     // Get the input
+//     const auto* input_tensor(ctx->Input<Tensor>(1));
+//     const InputT* input_data(input_tensor->Data<InputT>());
+
+//     // Prepare the output
+//     Tensor* output_tensor(ctx->Output(0, input_tensor->Shape()));
+//     std::string* output_data(output_tensor->MutableData<std::string>());
+
+//     // Execute
+//     const int64_t length(input_tensor->Shape().Size());
+
+//     for (int64_t i = 0; i < length; ++i) {
+//       output_data[i] = transformer.execute(input_data[i]);
+//     }
+//   }
+// };
+
+void StringTransformerImpl(OpKernelContext* ctx) {
     // Create the transformer
-    Microsoft::Featurizer::Featurizers::StringTransformer<InputT> transformer(
-        [ctx](void) {
+    Microsoft::Featurizer::Featurizers::StringTransformer transformer(
+        [ctx]() {
           const auto* state_tensor(ctx->Input<Tensor>(0));
           const uint8_t* const state_data(state_tensor->Data<uint8_t>());
 
           Microsoft::Featurizer::Archive archive(state_data, state_tensor->Shape().GetDims()[0]);
-          return Microsoft::Featurizer::Featurizers::StringTransformer<InputT>(archive);
+          return Microsoft::Featurizer::Featurizers::StringTransformer(archive);
         }());
 
     // Get the input
     const auto* input_tensor(ctx->Input<Tensor>(1));
-    const InputT* input_data(input_tensor->Data<InputT>());
+    const std::string* input_data(input_tensor->Data<std::string>());
 
     // Prepare the output
     Tensor* output_tensor(ctx->Output(0, input_tensor->Shape()));
@@ -39,7 +67,6 @@ struct StringTransformerImpl {
     for (int64_t i = 0; i < length; ++i) {
       output_data[i] = transformer.execute(input_data[i]);
     }
-  }
 };
 
 class StringTransformer final : public OpKernel {
@@ -48,10 +75,12 @@ class StringTransformer final : public OpKernel {
   }
 
   Status Compute(OpKernelContext* ctx) const override {
-    utils::MLTypeCallDispatcher<StringTransformerImpl, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t,
-                                int64_t, uint64_t, float, double, bool, std::string>
-        t_disp(ctx->Input<Tensor>(1)->GetElementType());
-    t_disp.Invoke(ctx);
+    // utils::MLTypeCallDispatcher<StringTransformerImpl, //int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t,
+    //                             //int64_t, uint64_t, float, double, bool,
+    //                             std::string>
+    //     t_disp(ctx->Input<Tensor>(1)->GetElementType());
+    // t_disp.Invoke(ctx);
+    StringTransformerImpl(ctx);
     return Status::OK();
   }
 };
@@ -63,18 +92,18 @@ ONNX_OPERATOR_KERNEL_EX(
     kCpuExecutionProvider,
     KernelDefBuilder()
         .TypeConstraint("T0", DataTypeImpl::GetTensorType<uint8_t>())
-        .TypeConstraint("InputT", {DataTypeImpl::GetTensorType<int8_t>(),
-                                   DataTypeImpl::GetTensorType<uint8_t>(),
-                                   DataTypeImpl::GetTensorType<int16_t>(),
-                                   DataTypeImpl::GetTensorType<uint16_t>(),
-                                   DataTypeImpl::GetTensorType<int32_t>(),
-                                   DataTypeImpl::GetTensorType<uint32_t>(),
-                                   DataTypeImpl::GetTensorType<int64_t>(),
-                                   DataTypeImpl::GetTensorType<uint64_t>(),
-                                   DataTypeImpl::GetTensorType<float>(),
-                                   DataTypeImpl::GetTensorType<double>(),
-                                   DataTypeImpl::GetTensorType<bool>(),
-                                   DataTypeImpl::GetTensorType<std::string>()}),
+        .TypeConstraint("InputT", //{DataTypeImpl::GetTensorType<int8_t>(),
+                                   //DataTypeImpl::GetTensorType<uint8_t>(),
+                                   //DataTypeImpl::GetTensorType<int16_t>(),
+                                   //DataTypeImpl::GetTensorType<uint16_t>(),
+                                   //DataTypeImpl::GetTensorType<int32_t>(),
+                                   //DataTypeImpl::GetTensorType<uint32_t>(),
+                                   //DataTypeImpl::GetTensorType<int64_t>(),
+                                   //DataTypeImpl::GetTensorType<uint64_t>(),
+                                   //DataTypeImpl::GetTensorType<float>(),
+                                   //DataTypeImpl::GetTensorType<double>(),
+                                   //DataTypeImpl::GetTensorType<bool>(),
+                                   DataTypeImpl::GetTensorType<std::string>()),
     StringTransformer);
 
 }  // namespace featurizers
