d09a5bfb08b601626ff2872644952cef8e37b1ff
Ye Wang <52801275+wangyems@users.noreply.github.com>
2020-04-15 11:45:29 -0700

    123456

--- onnxruntime/featurizers_ops/cpu_featurizers_kernels.cc
+++ onnxruntime/featurizers_ops/cpu_featurizers_kernels.cc
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 // tsettesttestt
+//ser
 #include "featurizers_ops/cpu_featurizers_kernels.h"
 
 #include "core/graph/constants.h"
